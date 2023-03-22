9f6121ce3bec2c3125035af04620d09a72682bed
Dikan Ruslan <Dikanb1502@gmail.com>
2023-03-22 17:32:38 +0200

    second-commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-
+<li>fff</li>
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<React.StrictMode></React.StrictMode>)
