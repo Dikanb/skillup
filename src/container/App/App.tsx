b4ad3556d5f980baf1999088ce4f52906117614d
Dikan Ruslan <Dikanb1502@gmail.com>
2023-03-23 16:31:15 +0200

    logo add

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,15 +1,18 @@
 import Footer from "container/Footer/Footer";
 import Main from "container/Main/Main";
 import Header from "container/Header/Header";
-
+import CssBaseline from '@mui/material/CssBaseline';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 type Props = {}
 const App =(props:Props) =>{
-  return (<div>
+  return (
+   <StyledEngineProvider injectFirst>
+   <CssBaseline/>
    <Header/>
    <Main/>
    <Footer/>
-  </div>
-  )
+   </StyledEngineProvider>
+   )
 }
 export default App
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,7 +1,34 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Container } from '@mui/material';
+import "./Header.scss"
+import Logo from 'components/Logo/Logo';
+
+
 type Props = {}
 const Header = (props: Props) => {
+
   return (
-    <div>Header</div>
-  )
+    <AppBar
+      position="static" className='app-bar'>
+  <Container>
+    <Toolbar>
+      <IconButton>
+        
+      </IconButton>
+      <Logo/>
+      <Button color="inherit">Home</Button>
+      <Button color="inherit">Abouth</Button>
+      <Button color="inherit">Payment</Button>
+      <Button color="inherit">Shiping</Button>
+      <Button color="inherit">Cart</Button>
+    </Toolbar>
+  </Container>
+  
+</AppBar >)
 }
 export default Header
