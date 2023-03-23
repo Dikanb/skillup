7309d13d2345b476b8e5b65c9c55d156e17854cf
Dikan Ruslan <Dikanb1502@gmail.com>
2023-03-23 17:11:29 +0200

    add menu

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@mui/material"
+
+
+
+type Props = {}
+const Menu = (props: Props) => {
+
+  return (
+  <>   
+  <Button color="inherit">Home</Button>
+  <Button color="inherit">Abouth</Button>
+  <Button color="inherit">Payment</Button>
+  <Button color="inherit">Shiping</Button>
+  <Button color="inherit">Cart</Button>
+  </>
+   
+  )
+}
+export default Menu
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,13 +1,12 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Container } from '@mui/material';
 import "./Header.scss"
 import Logo from 'components/Logo/Logo';
-
+import Menu from 'components/Menu/Menu';
+import CartHeader from 'components/CartHeader/CartHeader';
 
 type Props = {}
 const Header = (props: Props) => {
@@ -15,20 +14,23 @@ const Header = (props: Props) => {
   return (
     <AppBar
       position="static" className='app-bar'>
-  <Container>
-    <Toolbar>
-      <IconButton>
-        
-      </IconButton>
-      <Logo/>
-      <Button color="inherit">Home</Button>
-      <Button color="inherit">Abouth</Button>
-      <Button color="inherit">Payment</Button>
-      <Button color="inherit">Shiping</Button>
-      <Button color="inherit">Cart</Button>
-    </Toolbar>
-  </Container>
-  
-</AppBar >)
+      <Container>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Logo />
+          <Menu />
+          <CartHeader/>
+        </Toolbar>
+      </Container>
+
+    </AppBar >)
 }
 export default Header
